e713f82871a6bf00a83d192506b797796048bb42
Adan HZ <153233028+4d4n-HDZ@users.noreply.github.com>
2025-04-17 22:23:20 -0600

    Add files via upload

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,19 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+// Check if we're on the contact page
+if (
+  document.querySelector(".contact-page") &&
+  document.querySelector(".submit-button")
+) {
+  const contactPage = document.querySelector(".contact-page");
+  const submitButton = document.querySelector(".submit-button");
+
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form from submitting and reloading the page
+
+    // Replace content with thank-you message
+    contactPage.innerHTML =
+      '<p class="large-text">Thank you for your message</p>';
+  });
+}
